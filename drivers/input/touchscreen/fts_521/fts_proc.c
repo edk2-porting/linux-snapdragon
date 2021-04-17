d355bb74fb45dd39b6f9f4b727f6ab87979996d2
BigfootACA <bigfoot@classfun.cn>
2021-04-18 01:05:21 +0800

    Input: FTS_V521: fts_proc.c: Remove 'type' argument from access_ok() function
    Signed-off-by: BigfootACA <bigfoot@classfun.cn>

--- drivers/input/touchscreen/fts_521/fts_proc.c
+++ drivers/input/touchscreen/fts_521/fts_proc.c
@@ -355,7 +355,7 @@ static ssize_t fts_driver_test_write(struct file *file, const char __user *buf,
 	mess.action = 0;
 	mess.msg_size = 0;
 
-	if (access_ok(VERIFY_READ, buf, count) < OK
+	if (access_ok(buf, count) < OK
 	    || copy_from_user(pbuf, buf, count) != 0) {
 		res = ERROR_ALLOC;
 		goto END;
